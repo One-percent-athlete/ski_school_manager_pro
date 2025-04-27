4fc6624f921e8c4d63e15f1fac88b3aaca557042
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-27 18:00:44 +0900

    Update admin

--- manager_app/admin.py
+++ manager_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile, Lesson, Notification
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Lesson)
+admin.site.register(Notification)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
