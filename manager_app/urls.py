33b9f12a1a70b6b86077f041ac055ce20bf24725
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-28 22:29:43 +0900

    Update urls

--- manager_app/urls.py
+++ manager_app/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('schedule/', views.schedule, name='schedule'),
 
-    # path('register_user/', views.register_user, name="register_user"),
-    # path('login_user/', views.login_user, name="login_user"),
-    # path('logout_user/', views.logout_user, name="logout_user"),
-    # path('delete_user/<int:user_id>/', views.delete_user, name="delete_user"),
+    path('register_user/', views.register_user, name="register_user"),
+    path('login_user/', views.login_user, name="login_user"),
+    path('logout_user/', views.logout_user, name="logout_user"),
+    path('delete_user/<int:user_id>/', views.delete_user, name="delete_user"),
 
     # path('profile_list/', views.profile_list, name="profile_list"),
     # path('update_profile/<int:profile_id>/', views.update_profile, name="update_profile"),
